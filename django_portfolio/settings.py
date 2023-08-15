d25213489390cedc05d2295d578421a28bdd1845
agusrosero <agusrosero2000@gmail.com>
2023-08-15 08:20:16 -0300

    change settings and home.html

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "django_portfolio.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
