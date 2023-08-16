9357c481f324feed2a14d382eca7bb23f538f953
agusrosero <agusrosero2000@gmail.com>
2023-08-16 12:37:46 -0300

    changes and add button ELIMINAR

--- mi_blog/views.py
+++ mi_blog/views.py
@@ -10,3 +10,8 @@ def render_posts(request):
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, "post_detail.html", {"post": post})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    del_post = post.delete()
+    return render(request, "posts.html", {"del_post": del_post})
--- portfolio/views.py
+++ portfolio/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render
 from .models import Proyect
 
 
-# Definimos que hara cuando llegue a una url.
-# Render accede a cualquier carpeta llamada templates.
 def home(request):
-    # Trae todos los proyectos que estan en obyect:
     proyects = Proyect.objects.all()
     return render(request, "home.html", {"proyects": proyects})
