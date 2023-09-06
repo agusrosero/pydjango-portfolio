607514628dae6349ae328fd599577ab3d88d0d94
agusrosero <agusrosero2000@gmail.com>
2023-09-06 14:16:41 -0300

    suscribe method str in portfolio/models

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from .models import Proyect
 
-"""
-Registramos la clase Proyect (haciendo que se muestre el titulo, descripcion,
-image, url - como ya definimos en models.py - mediante el ingreso a www.localhost:8000.com/admin)
-"""
+
 admin.site.register(Proyect)
--- portfolio/models.py
+++ portfolio/models.py
@@ -9,3 +9,7 @@ class Proyect(models.Model):
     descripcion = CharField(max_length=250)
     image = ImageField(upload_to="portfolio/image")
     url = URLField(blank=True)
+
+    def __str__(self):
+        return self.titulo
+    
