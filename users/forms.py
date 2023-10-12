b2b528a20e8a7d0d3cf6538e792774d11c272fd6
agusrosero <agusrosero2000@gmail.com>
2023-10-12 15:08:16 -0300

    comments

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+# Login form
 class LoginForm(forms.Form):
     
     username = forms.CharField(max_length=60)
     password = forms.CharField(max_length=60, widget=forms.PasswordInput)
 
+# Register form
 class SignUpForm(UserCreationForm):
 
     class Meta:
